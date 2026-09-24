ee437518a7a625cd8b08079bbf7da65044b6c79f
Mieszko Mahboob <mieszko@mahboob.pl>
2026-09-24 21:18:59 +0200

    chore: update gateway address

--- public/config.js
+++ public/config.js
@@ -1,2 +1,2 @@
 // Publiczny adres HTTPS bramki. Nie wpisuj tutaj hasła ani tokenów.
-window.CHAT_GATEWAY_URL = "https://pete-printing-screening-reductions.trycloudflare.com";
+window.CHAT_GATEWAY_URL = "https://bidder-transmitted-wood-prefers.trycloudflare.com";
